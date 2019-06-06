210cbf98e087acd86c627a2ea041524e1849c7a3
Frank Vumbaca <fvumbaca@outlook.com>
2019-06-05 22:51:41 -0400

    Require alchemist and remeber theme settings

--- init.el
+++ init.el
@@ -1,3 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(package-selected-packages
+   (quote
+    (alchemist elixir-mode powerline projectile spacemacs-theme evil-leader evil org-plus-contrib use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (let ((gc-cons-threshold most-positive-fixnum))
 
   ;; Set repositories
@@ -23,20 +41,3 @@
   ;; Tangle configuration
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
   (garbage-collect))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages
-   (quote
-    (alchemist elixir-mode powerline projectile spacemacs-theme evil-leader evil org-plus-contrib use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
