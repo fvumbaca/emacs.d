a83c04fb88f4fee557e8513e4fef9496765aeac5
Frank Vumbaca <fvumbaca@outlook.com>
2019-07-07 16:35:20 -0400

    Added basic go and moved some evil leader keys

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (kubernetes-evil kubernetes dockerfile-mode yaml-mode clojure-mode-extra-font-locking evil-paredit clojure-mode paredit rainbow-delimiters ido-completing-read+ cider flycheck slime emojify ace-window exec-path-from-shell alchemist elixir-mode powerline projectile spacemacs-theme evil-leader evil org-plus-contrib use-package))))
+    (go-mode markdown-mode kubernetes-evil kubernetes dockerfile-mode yaml-mode clojure-mode-extra-font-locking evil-paredit clojure-mode paredit rainbow-delimiters ido-completing-read+ cider flycheck slime emojify ace-window exec-path-from-shell alchemist elixir-mode powerline projectile spacemacs-theme evil-leader evil org-plus-contrib use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
